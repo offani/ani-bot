f81a85d9c86db73bbb6c62c64e5a92938c4b92bf
Aniketh Gaonkar <87714033+offani@users.noreply.github.com>
2025-08-19 09:47:16 +0530

    Update c.py

--- c.py
+++ c.py
@@ -2,7 +2,7 @@ from langchain.llms import Groq
 from langchain_core.prompts import PromptTemplate
 from langgraph.graph import StateGraph, END
 from pydantic import Basemodel
-import os
+
 
 from python_dotenv import dotenv
 
